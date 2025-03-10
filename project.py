d2ea7c2f85292b15bdffadec3c67bcccbc044966
athenova <aleksey.sergeyev@yandex.com>
2025-03-10 11:18:40 +0300

    Начало проекта

---
+++ project.py
@@ -0,0 +1,35 @@
+from simple_blogger import Journalist
+from simple_blogger.generators.YandexGenerator import YandexImageGenerator
+from simple_blogger.generators.YandexGenerator import YandexTextGenerator
+from datetime import datetime
+
+class Project(Journalist):
+    def _system_prompt(self, _):
+        return "Ты - специалист по животным, блоггер с 1000000 подписчиков, умеющий заинтересовать аудиторию в изучении животного мира"
+
+    def _example_task_creator(self):
+        return [ {"family": "Family", "genus": "Genus", "species": "Species"} ]
+
+    def _get_category_folder(self, task):
+        return f"{task['family']}/{task['genus']}"
+
+    def _get_topic_folder(self, task):
+        return task['species']
+
+    def _task_converter(self, idea):
+        return { 
+                    "family": idea['family'],
+                    "genus": idea['genus'],
+                    "species": idea['species'],
+                    "topic_image": f"Нарисуй животное породы '{idea['species']}' из рода '{idea['genus']}' семейства '{idea['family']}'. Эстетично, красиво, реалистично, крупным планом",
+                    "topic_prompt": f"Расскажи интересный факт о животном породы '{idea['species']}' из рода '{idea['genus']}' семейства '{idea['family']}'",
+                }
+
+    def __init__(self, **kwargs):
+        super().__init__(
+            review_chat_id=-1002374309134,
+            first_post_date=datetime(2025, 2, 18),
+            text_generator=YandexTextGenerator(),
+            image_generator=YandexImageGenerator(),
+            **kwargs
+        )
