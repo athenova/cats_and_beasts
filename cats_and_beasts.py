7003ec437bca189f001d090d24d80279fed4d665
athenova <aleksey.sergeyev@yandex.com>
2025-04-05 21:10:51 +0300

    Добавил теги

--- cats_and_beasts.py
+++ cats_and_beasts.py
@@ -3,7 +3,11 @@ from simple_blogger.poster.TelegramPoster import TelegramPoster
 from simple_blogger.poster.VkPoster import VkPoster
 from simple_blogger.poster.InstagramPoster import InstagramPoster
 from simple_blogger.editor import Editor
+from simple_blogger.preprocessor.text import TagAdder
 from datetime import date, timedelta
+
+tagadder = TagAdder(['#вмиреживотных', '#животныймир'])
+first_post_date=date(2025, 3, 11)
 
 class AnimalBlogger(AutoBlogger):
     def _system_prompt(self):
@@ -20,19 +24,19 @@ class AnimalBlogger(AutoBlogger):
         
     def _posters(self):
         return [
-            TelegramPoster(chat_id='@cats_and_beasts'),
-            VkPoster(group_id='229821868'),
-            InstagramPoster(account_token_name='CATS_AND_BEASTS_TOKEN')
+            TelegramPoster(chat_id='@cats_and_beasts', processor=tagadder),
+            VkPoster(group_id='229821868', processor=tagadder),
+            InstagramPoster(account_token_name='CATS_AND_BEASTS_TOKEN', processor=tagadder)
         ]
 
-    def __init__(self, posters=None, first_post_date=date(2025, 3, 11), force_rebuild=False):
+    def __init__(self, posters=None, first_post_date=first_post_date, force_rebuild=False):
         super().__init__(posters=posters or self._posters(), first_post_date=first_post_date, force_rebuild=force_rebuild)
 
 def review():
     day_to_review = 1
     blogger = AnimalBlogger(
-        posters=[TelegramPoster()],
-        first_post_date=date(2025, 3, 11)-timedelta(days=day_to_review),
+        posters=[TelegramPoster(processor=tagadder)],
+        first_post_date=first_post_date-timedelta(days=day_to_review),
         force_rebuild=True
     )
     blogger.post()
@@ -44,4 +48,4 @@ def post():
 def init():
     editor = Editor()
     editor.init_project()
-    editor.create_auto(first_post_date=date(2025, 3, 11)+timedelta(days=131))
+    editor.create_auto(first_post_date=first_post_date+timedelta(days=131))
